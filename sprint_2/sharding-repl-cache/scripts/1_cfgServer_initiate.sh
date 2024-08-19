d54338ff3bcee71156fe9c164f38f87e3922c180
rtszh <arthur.sazhnev@yandex.ru>
2024-08-19 20:51:45 +0300

    hw_sprint2_2

---
+++ sprint_2/sharding-repl-cache/scripts/1_cfgServer_initiate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###
+# создание replicaset для configserver
+###
+docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+EOF
+
